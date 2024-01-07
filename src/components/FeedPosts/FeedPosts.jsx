082b51cfa70e3503ef11f5f98d8278a7e6a6d566
FransiskusAriyanto <fransiskusariyanto2400@gmail.com>
2024-01-07 19:44:08 +0700

    January 7, 2024. 7.43pm

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -4,10 +4,10 @@ import FeedPost from "./FeedPost";
 const FeedPosts = () => {
   return (
     <Container maxW={"container.sm"} py={10} px={2}>
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
+      <FeedPost img='/img1.png' username='frns.ari' avatar='profilepic.JPG' />
+      <FeedPost img='/img2.png' username='hehe' avatar='img2.png' />
+      <FeedPost img='/img3.png' username='bekantan' avatar='img3.png' />
+      <FeedPost img='/img4.png' username='uncle mutu' avatar='img4.png' />
     </Container>
   );
 };
